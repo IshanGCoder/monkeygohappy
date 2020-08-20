0579a7836f71c5b2eeee3324f342f361f68e68d0
IshanGCoder <69946511+IshanGCoder@users.noreply.github.com>
2020-08-20 11:38:29 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var bananaImage, obstacleImage, obstacleGroup, backImage, score;
+
+function preload () {
+  backImage = loadImage("jungle.png");
+  monkeyRunning = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png", "Monkey_04.png", "Monkey_05.png", "Monkey_06.png", "Monkey_07.png", "Monkey_08.png", "Monkey_09.png", "Monkey_10.png");
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("stone.png");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  back = createSprite(200,200,400,400);
+  back.addImage("jungle.png", backImage);
+  back.velocityX = -5
+  
+}
+
+function draw() {
+  background(220);
+}
